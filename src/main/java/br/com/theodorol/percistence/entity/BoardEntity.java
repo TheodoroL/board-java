563531e29670b079041a12c9040450f96c2e0711
Lucas <lucastheodoromourofernandes@gmail.com>
2025-04-27 01:47:33 -0400

    Camada de DTO

--- src/main/java/br/com/theodorol/percistence/entity/BoardEntity.java
+++ src/main/java/br/com/theodorol/percistence/entity/BoardEntity.java
@@ -3,7 +3,9 @@ package br.com.theodorol.percistence.entity;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.function.Predicate;
 
+import static br.com.theodorol.percistence.entity.BoardColumnKindEnum.CANCEL;
 import static br.com.theodorol.percistence.entity.BoardColumnKindEnum.INITIAL;
 
 public class BoardEntity {
@@ -38,6 +40,16 @@ public class BoardEntity {
     public BoardColumnsEntity getInitialColumn(){
         return boardColumns.stream()
                 .filter(bc -> bc.getKind().equals(INITIAL))
+                .findFirst().orElseThrow();
+    }
+
+    public BoardColumnsEntity getCancelColumn(){
+        return getFilteredColumn(bc -> bc.getKind().equals(CANCEL));
+    }
+
+    private BoardColumnsEntity getFilteredColumn(Predicate<BoardColumnsEntity> filter){
+        return boardColumns.stream()
+                .filter(filter)
                 .findFirst().orElseThrow();
     }
 }
--- src/main/java/br/com/theodorol/ui/BoardMenu.java
+++ src/main/java/br/com/theodorol/ui/BoardMenu.java
@@ -87,7 +87,19 @@ public class BoardMenu {
     private void blockCard() {
     }
 
-    private void cancelCard() {
+    private void cancelCard() throws SQLException {
+        System.out.println("Informe o id do card que deseja mover para a  coluna de cancelamento");
+        var cardId = scanner.nextLong();
+        var cancelColumn = entity.getCancelColumn();
+        var boardColumnsInfo = entity.getBoardColumns().stream()
+                .map(bc -> new BoardColumnInfoDTO(bc.getIdBoardColum(), bc.getOrder(), bc.getKind()))
+                .toList();
+        try(var connection = getConnection()){
+            new CardService(connection).cancel(cardId, cancelColumn.getIdBoardColum(), boardColumnsInfo);
+        } catch (RuntimeException ex){
+            System.out.println(ex.getMessage());
+        }
+
     }
 
     private void showBoard() throws SQLException {
